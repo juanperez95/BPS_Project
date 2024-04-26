8d39bdea9bed5cb5e62648cc0509e0566a749a6a
Sanfor20 <santiteus2005@gmail.com>
2024-04-26 09:16:11 -0500

    Controller

--- Inv-Asignacion/app/Http/Controllers/Controller.php
+++ Inv-Asignacion/app/Http/Controllers/Controller.php
@@ -5,4 +5,15 @@ namespace App\Http\Controllers;
 abstract class Controller
 {
     //
+    $proveedor = Proveedor::create([
+        'id_proveedor' => '0001',
+        'nombre' => 'PYM Mayorista',
+        'nit' => '123456789',
+        'direccion' => 'Calle 123',
+        'telefono' => '3000000000',
+        'email' => 'pymmayorista@gmail.com.com',
+        'estado' => 'Activo',
+        'created_at' => date('Y-m-d H:i:s'),
+        'updated_at' => date('Y-m-d H:i:s'),
+      ]);
 }
